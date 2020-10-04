ea3606ddf64a02a2a77bcc98e9eeb86d454334b4
Toby Bell <tbell@stanford.edu>
2020-10-03 23:28:01 -0600

    Add usage error and help text to CLI

--- src/main.c
+++ src/main.c
@@ -1,10 +1,30 @@
 #include <sha256/sha256.h>
 
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 unsigned char buf[4096];
 
-int main(int argc, const char* argv[]) {
+int help() {
+  printf(
+    "Usage: sha256 [OPTIONS]\n\n"
+    "Compute the SHA 256 digest of a message.\n\n"
+    "The message will be read from standard input, and the binary\n"
+    "digest will be written to standard output.\n\n"
+    "Options:\n"
+    "  --help / -h  Show this message and exit\n");
+  return 0;
+}
+
+int usage() {
+  printf(
+    "Usage: sha256 [OPTIONS]\n"
+    "See 'sha256 --help'.\n");
+  return 1;
+}
+
+int hash() {
   sha256_ctx c;
   sha256_init(&c);
   while (1) {
@@ -16,4 +36,17 @@ int main(int argc, const char* argv[]) {
   sha256_final(&d, &c);
   write(STDOUT_FILENO, &d, sizeof (d));
   return 0;
+}
+
+int main(int argc, const char* argv[]) {
+  if (argc > 2) {
+    return usage();
+  }
+  if (argc > 1 && (!strcmp(argv[1], "--help") || !strcmp(argv[1], "-h"))) {
+    return help();
+  }
+  if (argc > 1) {
+    return usage();
+  }
+  return hash();
 }
